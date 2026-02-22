57419920c2d213b983e3e8f697c41ca763f8756c
Lindahl2010 <lindahl2010@gmail.com>
2026-02-22 09:20:26 -0600

    2015: day 7 (part 2) unit test.

--- ts/src/2015/tests/day7.test.ts
+++ ts/src/2015/tests/day7.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, test } from "vitest";
+import { describe, expect, it } from "vitest";
 import { Day7 } from "../day7.js";
 
 describe("2015: day 6", () => {
@@ -22,7 +22,21 @@ describe("2015: day 6", () => {
     expect(result).toBe(65527);
   });
 
-  test.each([])("part2", (input, expected) => {
-    expect(day.part2(input)).toBe(expected);
+  it("part2", () => {
+    // given
+    const input = [
+      "123 -> x",
+      "456 -> b",
+      "x AND b -> d",
+      "d RSHIFT 5 -> lo",
+      "lo LSHIFT 2 -> f",
+      "NOT f -> a",
+    ].join("\n");
+
+    // when
+    const result = day.part2(input);
+
+    // then
+    expect(result).toBe(65523);
   });
 });
