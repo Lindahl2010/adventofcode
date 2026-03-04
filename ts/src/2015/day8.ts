df410a7bf424aec41249c79ad2bcd06dc4c3d1b8
Lindahl2010 <lindahl2010@gmail.com>
2026-03-04 17:54:12 -0600

    2015: day 8 (part 2) solution.

--- ts/src/2015/day8.ts
+++ ts/src/2015/day8.ts
@@ -12,7 +12,10 @@ export class Day8 extends Puzzle {
       .reduce((prev, curr) => prev + curr, 0);
   }
 
-  public override part2(_input: string): number {
-    return 0;
+  public override part2(input: string): number {
+    const data = input.split("\n");
+    return data
+      .map((line) => JSON.stringify(line).length - line.length)
+      .reduce((prev, curr) => prev + curr, 0);
   }
 }
